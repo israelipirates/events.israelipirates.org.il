32fca51b7d9f9f551e852284cce9607e57da31a6
Yuval Adam <yuv.adm@gmail.com>
2012-08-24 16:18:57 +0300

    switch to DATABASE_URL

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_RED_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
